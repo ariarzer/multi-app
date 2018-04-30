61e8c9cc4a54a2cd5faec1512d4427f9128a3b78
Sofi Valitova <ariarzer@gmail.com>
2018-04-30 12:37:46 +0300

    Primary markup (#1)
    * Init branch
    
    * Add the color
    
    * Style for header
    
    * Remove underscore links
    
    * Indentations added
    
    * Add style for active and hover links
    
    * Returned a dotted border around links in focus
    
    * Header became class
    
    * Colors are stored in variables

--- src/components/root.vue
+++ src/components/root.vue
@@ -1,13 +1,101 @@
 <template>
-  <div>
+  <div class="multi-app">
 
-    <header>Multifunctional application</header>
+    <header class="header">Multifunctional application</header>
 
-    <div>
-      <router-link to="/app1">App1</router-link>
-      <router-link to="/app2">App2</router-link>
+    <div class="sidebar">
+      <router-link to="/app1" class="link">App1</router-link>
+      <router-link to="/app2" class="link">App2</router-link>
     </div>
 
-    <router-view></router-view>
+    <router-view class="app"></router-view>
   </div>
 </template>
+
+<style>
+  html,
+  body {
+    padding: 0px;
+    margin: 0px;
+  }
+
+  body {
+    width: 100vw;
+    height: 100vh;
+    font-size: 20px;
+  }
+
+  .multi-app {
+    --indent: 15px;
+
+    --active-green: rgba(157, 222, 91, 0.25);
+    --hover-green: rgba(157, 222, 91, 0.5);
+    --light-green: rgb(157, 222, 91);
+    --dark-green: rgb(36, 54, 21);
+    --dark-grey: rgb(227, 228, 232);
+
+    display: grid;
+    grid-template-rows: 10% auto;
+    grid-template-columns: minmax(170px, 15%) 1fr;
+    grid-template-areas: "header header" "sidebar app";
+
+    width: 100%;
+    height: 100%;
+
+    background-color: rgb(237, 238, 240);
+  }
+
+  .header,
+  .link,
+  .app {
+    color: var(--dark-green);
+  }
+
+  .header,
+  .app {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .header {
+    grid-area: header;
+
+    background-color: var(--light-green);
+
+    font-size: 30px;
+    font-weight: bolder;
+  }
+
+  .sidebar {
+    grid-area: sidebar;
+
+    display: flex;
+    flex-direction: column;
+
+    margin-top: var(--indent);
+  }
+
+  .link {
+    text-decoration: none;
+
+    padding: var(--indent);
+  }
+
+  .link:hover {
+    background-color: var(--hover-green);
+  }
+
+  .router-link-active {
+    background-color: var(--active-green);
+  }
+
+  .app {
+    grid-area: app;
+
+    background-color: white;
+    border: 1px solid var(--dark-grey);
+
+    margin: var(--indent);
+  }
+</style>
